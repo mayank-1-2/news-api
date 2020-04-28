aa566a58c7e42c398629be7cdfa11b19e5389807
mayank-1-2 <60282634+mayank-1-2@users.noreply.github.com>
2020-04-28 13:51:16 +0530

    Version 1

---
+++ news.js
@@ -0,0 +1,27 @@
+let news=document.getElementById('news')
+
+const xhr= new XMLHttpRequest();
+xhr.open('GET','http://newsapi.org/v2/top-headlines?country=in&apiKey=7c70eaa46034410f9151aac41a9bee38',true);
+
+xhr.onload = function () {
+    if (this.status==200) {
+        let json=JSON.parse(this.responseText);
+        newshtml="";
+        json['articles'].forEach(function(element) {
+            let img=element.urlToImage;
+            item=`<div class="card">
+             <img src="${img}" alt="">
+            <div class="title">${element.title}</div>
+            <div class="desc">${element.description}</div>
+            <div class="url"><a href=${element['url']} target="_blank">Read more</a>></div>
+            </div>`
+         newshtml+=item;
+        });
+        news.innerHTML=newshtml;
+    }
+    else{
+        console.log('Some error occured')
+    }
+    
+}
+xhr.send();
